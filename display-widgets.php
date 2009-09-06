8c53e32dce33b80305bda33fb8192e83310617d5
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-06 15:40:43 +0000

    Fix display widget options
    git-svn-id: https://plugins.svn.wordpress.org/display-widgets/tags/1.1@152516 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-widgets.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: Display widgets
+Plugin URI: http://blog.strategy11.com/2009/09/06/display-widgets/
+Description: Adds checkboxes to each widget to show or hide on site pages.
+Author: Stephanie Wells
+Author URI: http://ionixdesign.com 
+Version: 1.1
+*/
+
+function show_dw_widget($instance){
+    $post_id = $GLOBALS['post']->ID;
+    $show = $instance['page-'.$post_id]; 
+    if (($instance['include'] and $show == false) or ($instance['include'] == 0 and $show))
+        return false;
+    else
+        return $instance;
+}
+
+function dw_show_hide_widget_options($widget, $return, $instance){ 
+    $pages = get_posts( array('post_type' => 'page', 'post_status' => 'published', 'numberposts' => 99, 'order_by' => 'post_title', 'order' => 'ASC'));
+    
+    $instance['include'] = $instance['include'] ? $instance['include'] : 0;
+?>   
+     <p>
+    	<label for="<?php echo $widget->get_field_id('include'); ?>">Show/Hide Widget on the following pages</label>
+    	<select name="<?php echo $widget->get_field_name('include'); ?>" id="<?php echo $widget->get_field_id('include'); ?>" class="widefat">
+            <option value="1" <?php echo selected( $instance['include'], 1 ) ?>>Show on checked</option>
+            <option value="0" <?php echo selected( $instance['include'], 0 ) ?>>Hide on checked</option> 
+        </select>
+    </p>    
+
+<div style="height:150px; overflow:auto; border:1px solid #dfdfdf;">
+    <?php foreach ($pages as $page){ 
+        $instance['page-'.$page->ID] = $instance['page-'.$page->ID] ? $instance['page-'.$page->ID] : false;
+            
+    ?>
+        <p><input class="checkbox" type="checkbox" <?php checked($instance['page-'.$page->ID], true) ?> id="<?php echo $widget->get_field_id('page-'.$page->ID); ?>" name="<?php echo $widget->get_field_name('page-'.$page->ID); ?>" />
+        <label for="<?php echo $widget->get_field_id('page-'.$page->ID); ?>"><?php _e($page->post_title) ?></label></p>
+    <?php	} 
+echo '</div>';        
+}
+
+
+add_filter('widget_display_callback', 'show_dw_widget');
+add_action('in_widget_form', 'dw_show_hide_widget_options', 10, 3);
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== Display Widgets ===
+Contributors: sswells
+Donate link: http://blog.strategy11.com/donate/
+Tags: widgets, show, hide, Thesis, sidebar
+Requires at least: 2.8
+Tested up to: 2.8.4
+Stable tag: 1.1
+
+Adds checkboxes to each widget to either show or hide on every site page.
+
+== Description ==
+
+Avoid creating multiple sidebars and duplicating widgets by adding check boxes to each widget which will either show or hide the widgets on every site page. Great for use with Thesis theme, or just to avoid extra coding.
+
+By default, 'Hide on Checked' is selected with no boxes checked, so all current widgets will continue to display on all pages. 
+
+== Installation ==
+
+1. Upload `display-widgets.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the 'Widgets' menu and show the options panel for the widget you would like to hide.
+4. Select either 'Show on Checked' or 'Hide on Checked' from the drop-down and check the boxes.
+
+== Screenshots ==
+
+1. The extra widget options added.
+
