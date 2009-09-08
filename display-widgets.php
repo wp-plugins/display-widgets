55f3c53938827d19d80f09eff1d4abbce5221287
sswells <sswells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-08 22:05:26 +0000

    added more checkboxes
    git-svn-id: https://plugins.svn.wordpress.org/display-widgets/tags/1.3@153189 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ display-widgets.php
@@ -0,0 +1,84 @@
+<?php
+/*
+Plugin Name: Display widgets
+Plugin URI: http://blog.strategy11.com/2009/09/06/display-widgets/
+Description: Adds checkboxes to each widget to show or hide on site pages.
+Author: Stephanie Wells
+Author URI: http://ionixdesign.com 
+Version: 1.3
+*/
+
+function show_dw_widget($instance){
+    $post_id = $GLOBALS['post']->ID;
+    if (get_option('show_on_front') == 'posts' and is_home())
+        $show = $instance['page-home'];
+    else if (is_archive())
+        $show = $instance['page-archive'];
+    else if (is_single())    
+        $show = $instance['page-single'];
+    else
+        $show = $instance['page-'.$post_id]; 
+    
+    if (($instance['include'] and $show == false) or ($instance['include'] == 0 and $show))
+        return false;
+    else
+        return $instance;
+}
+
+function dw_show_hide_widget_options($widget, $return, $instance){ 
+    $pages = get_posts( array('post_type' => 'page', 'post_status' => 'published', 'numberposts' => 99, 'order_by' => 'post_title', 'order' => 'ASC'));
+    
+    $instance['include'] = $instance['include'] ? $instance['include'] : 0;
+?>   
+     <p>
+    	<label for="<?php echo $widget->get_field_id('include'); ?>">Show/Hide Widget</label>
+    	<select name="<?php echo $widget->get_field_name('include'); ?>" id="<?php echo $widget->get_field_id('include'); ?>" class="widefat">
+            <option value="0" <?php echo selected( $instance['include'], 0 ) ?>>Hide on checked</option> 
+            <option value="1" <?php echo selected( $instance['include'], 1 ) ?>>Show on checked</option>
+        </select>
+    </p>    
+
+<div style="height:150px; overflow:auto; border:1px solid #dfdfdf;">
+    <?php 
+    if (get_option('show_on_front') == 'posts' ){
+        $instance['page-home'] = $instance['page-home'] ? $instance['page-home'] : false;
+    
+    $instance['page-archive'] = $instance['page-archive'] ? $instance['page-archive'] : false;
+    $instance['page-single'] = $instance['page-single'] ? $instance['page-single'] : false;    
+    ?>
+        <p><input class="checkbox" type="checkbox" <?php checked($instance['page-home'], true) ?> id="<?php echo $widget->get_field_id('page-home'); ?>" name="<?php echo $widget->get_field_name('page-home'); ?>" />
+        <label for="<?php echo $widget->get_field_id('page-home'); ?>"><?php _e('Home Page') ?></label></p>
+    <?php
+    }
+    foreach ($pages as $page){ 
+        $instance['page-'.$page->ID] = $instance['page-'.$page->ID] ? $instance['page-'.$page->ID] : false;   
+    ?>
+        <p><input class="checkbox" type="checkbox" <?php checked($instance['page-'.$page->ID], true) ?> id="<?php echo $widget->get_field_id('page-'.$page->ID); ?>" name="<?php echo $widget->get_field_name('page-'.$page->ID); ?>" />
+        <label for="<?php echo $widget->get_field_id('page-'.$page->ID); ?>"><?php _e($page->post_title) ?></label></p>
+    <?php	}  ?>
+    
+    <p><input class="checkbox" type="checkbox" <?php checked($instance['page-archive'], true) ?> id="<?php echo $widget->get_field_id('page-archive'); ?>" name="<?php echo $widget->get_field_name('page-archive'); ?>" />
+    <label for="<?php echo $widget->get_field_id('page-archive'); ?>"><?php _e('Archives Page') ?></label></p>
+
+    <p><input class="checkbox" type="checkbox" <?php checked($instance['page-single'], true) ?> id="<?php echo $widget->get_field_id('page-single'); ?>" name="<?php echo $widget->get_field_name('page-single'); ?>" />
+    <label for="<?php echo $widget->get_field_id('page-single'); ?>"><?php _e('Single Post Page') ?></label></p>
+<?php
+echo '</div>';        
+}
+
+function dw_update_widget_options($instance, $new_instance, $old_instance){
+    $pages = get_posts( array('post_type' => 'page', 'post_status' => 'published', 'numberposts' => 99, 'order_by' => 'post_title', 'order' => 'ASC'));
+    foreach ($pages as $page)
+        $instance['page-'.$page->ID] = $new_instance['page-'.$page->ID] ? true : false;
+    $instance['include'] = $new_instance['include'];
+    $instance['page-home'] = $new_instance['page-home'];
+    $instance['page-archive'] = $new_instance['page-archive'];
+    $instance['page-single'] = $new_instance['page-single'];
+    return $instance;
+}
+
+
+add_filter('widget_display_callback', 'show_dw_widget');
+add_action('in_widget_form', 'dw_show_hide_widget_options', 10, 3);
+add_filter('widget_update_callback', 'dw_update_widget_options', 10, 3)
+?>
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== Display Widgets ===
+Contributors: sswells
+Donate link: http://blog.strategy11.com/donate/
+Tags: widgets, show, hide, Thesis, sidebar
+Requires at least: 2.8
+Tested up to: 2.8.4
+Stable tag: 1.3
+
+Adds checkboxes to each widget to either show or hide on every site page.
+
+== Description ==
+
+Avoid creating multiple sidebars and duplicating widgets by adding check boxes to each widget (as long as it is written in the version 2.8 format) which will either show or hide the widgets on every site page. Great for use with Thesis theme, or just to avoid extra coding.
+
+By default, 'Hide on Checked' is selected with no boxes checked, so all current widgets will continue to display on all pages. 
+
+== Installation ==
+
+1. Upload `display-widgets.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to the 'Widgets' menu and show the options panel for the widget you would like to hide.
+4. Select either 'Show on Checked' or 'Hide on Checked' from the drop-down and check the boxes.
+
+
+== Frequently Asked Questions ==
+
+= Why aren't the options showing up on my widget? =
+
+This is a known limitation. Widgets written in the pre-2.8 format don't work the same way, and don't have the hooks. Sorry.
+
+
+== Screenshots ==
+
+1. The extra widget options added.
+
+== Changelog ==
+
+= 1.3 =
+* Added check box for Home page if it is the blog page
+* Added check boxes for single post and archive pages
+* Save hide/show option correctly for more widgets
+
+= 1.2 =
+* Save page check boxes for more widgets
+
+= 1.1 =
+* Fixed bug that prevented other widget options to be displayed
+
